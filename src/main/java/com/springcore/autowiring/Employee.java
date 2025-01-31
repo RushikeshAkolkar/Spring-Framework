574e85bc9689a1e59f43c161ef0d3fa7b264ee1b
Rushikesh Akolkar <rushikeshakolkar150@gmail.com>
2025-01-31 16:55:11 +0530

    com.springcore.autowiring in this package i created auto wire example

---
+++ src/main/java/com/springcore/autowiring/Employee.java
@@ -0,0 +1,31 @@
+package com.springcore.autowiring;
+
+public class Employee {
+	
+	private Address address;//variable name in class file and config.xml file should by same for using autowiring byName
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		address = address;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(Address address) {
+		super();
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [address=" + address + "]";
+	}
+	
+
+}
