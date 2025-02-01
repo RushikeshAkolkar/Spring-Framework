d1da64f90f16479077a6b478b86e67a55a117779
Rushikesh Akolkar <rushikeshakolkar150@gmail.com>
2025-02-01 09:42:20 +0530

    @Qualifier annotation use to call a particular value to inject object value

--- src/main/java/com/springcore/autowiring/annotation/Employee.java
+++ src/main/java/com/springcore/autowiring/annotation/Employee.java
@@ -1,9 +1,11 @@
 package com.springcore.autowiring.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Employee {
 	@Autowired
+	@Qualifier("address2")  //if multiple beans of same class having values in that case we have to use
 	private Address address;//variable name in class file and config.xml file should by same for using autowiring byName
 
 	public Address getAddress() {
