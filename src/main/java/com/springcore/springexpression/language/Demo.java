9a1773245ccaf47b4a664affe1c9a2b24595ad59
Rushikesh Akolkar <rushikeshakolkar150@gmail.com>
2025-02-02 09:55:45 +0530

    added programe to access static variable value in same package

--- src/main/java/com/springcore/springexpression/language/Demo.java
+++ src/main/java/com/springcore/springexpression/language/Demo.java
@@ -9,6 +9,17 @@ public class Demo {
 	private int x;
 	@Value("#{33+44}")   	//this type of value is called as spring expression language
 	private int y;
+	@Value("#{T(java.lang.Math).sqrt(25)}")		//call static method of predefine class. you can check using shift+ctrl+t after pressing type package name and see.
+	private double z;
+	@Value("#{T(java.lang.Math).PI}")		// access static variable value
+	private double e;
+	
+	public double getE() {
+		return e;
+	}
+	public void setE(double e) {
+		this.e = e;
+	}
 	public int getX() {
 		return x;
 	}
@@ -21,9 +32,15 @@ public class Demo {
 	public void setY(int y) {
 		this.y = y;
 	}
+	public double getZ() {
+		return z;
+	}
+	public void setZ(double z) {
+		this.z = z;
+	}
 	@Override
 	public String toString() {
-		return "Demo [x=" + x + ", y=" + y + "]";
+		return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", e=" + e + "]";
 	}
 	
 	
